ec11a4dc6cdfe5b9e0c78f2c1a295d9e40bd085a
Leonardocregis <leonardocregis@gmail.com>
2018-06-09 10:23:23 -0300

    Feat refactored to use BookStorage controller for indexDb class

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import BookWardrobe from './components/wardrobe/BookWardrobe';
 import {Route} from 'react-router-dom';
 import './styles/App.css';
 import BookSearcher from './components/book/BookSearcher';
+import BookStorage from './database/BookStorage';
 
 class BooksApp extends React.Component {
 
   bookShelves = new Map();
+  bookStorage = new BookStorage('myShelf');
+
   constructor(props){
     super(props);
-    
-    this.bookShelves = bookStorage.bookShelves;
     this.readyState();
   }
 
@@ -19,7 +20,19 @@ class BooksApp extends React.Component {
     this.state = {
       bookShelves: this.bookShelves
     };
-  }   
+  }
+
+  componentDidMount() {
+    this.bookStorage.loadFromDb()
+      .then( result => {
+        let map = new Map();
+        result.forEach(element => {
+          map.set(element.title, element.books);
+        });
+        this.setState({bookShelves:map});
+      })
+      .catch( err => console.log(err));
+  }
 
   changeShelf = (fromShelf, toShelf, book) => {
 
