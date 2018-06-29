25ca731959a740825e35378335a1c4cc893c8c6d
LRegis <leonardo.regis@sicpa.com>
2018-06-29 10:37:36 -0300

    refact index.js: added implementation for app utility classes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
+import BookStorage from './database/BookStorage';
+import BookUtils from "./components/book/BookUtils";
+
+const bookShelves = new Map();
+const bookStorage = new BookStorage('myShelf');
+const bookUtils = new BookUtils();
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <App 
+            bookShelves={bookShelves}
+            bookStorage={bookStorage}
+            bookUtils={bookUtils}
+        />
     </BrowserRouter>, document.getElementById('root'))
