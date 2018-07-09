6df0640246c798fe3e1ecccf212bc65ef86c9e14
LRegis <leonardo.regis@sicpa.com>
2018-07-09 16:03:46 -0300

    refact covering cases where shelf is undefined

--- src/components/shelf/BookShelf.js
+++ src/components/shelf/BookShelf.js
@@ -9,29 +9,29 @@ class BookShelf extends React.Component {
 
   render() {
     const {shelf, availableActions} = this.props.shelf;
-    return (
-      <div className="bookshelf" data-testid="book-shelf">
-        <h2 className="bookshelf-title">{shelf.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              shelf.books && shelf.books.map( book => {
-                return (
-                  <li key={book.title}>
-                    <BookItem
-                      imageURL={book.imageLinks.thumbnail}
-                      book={book}
-                      availableActions={availableActions}
-                      changeShelf={this.changeShelf}
-                    />
-                  </li>
-                )
-              })
-            }
-          </ol>
-        </div>
+    if (shelf) {
+      return this.renderShelf(shelf, availableActions);
+    } else {
+      return (
+        <div> Undefined Shelf </div>
+      );
+    }
+
+  }
+
+  renderShelf(shelf, availableActions) {
+    return (<div className="bookshelf" data-testid="book-shelf">
+      <h2 className="bookshelf-title">{shelf.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {shelf.books && shelf.books.map(book => {
+            return (<li key={book.title}>
+              <BookItem imageURL={book.imageLinks.thumbnail} book={book} availableActions={availableActions} changeShelf={this.changeShelf} />
+            </li>);
+          })}
+        </ol>
       </div>
-    );
+    </div>);
   }
 };
 
